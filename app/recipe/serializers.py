2575b5ea8771870a2505e81c4d9858c7ac8c841b
izhan9637 <izhan9637@gmail.com>
2020-03-18 10:28:58 +0530

    Added Serializer for

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from core.models import Tag, Ingredient, Recipe
 
-
+#Serializer for tag object
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag object"""
 
